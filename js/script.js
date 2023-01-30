dab668520567fb2cd778abcc4e7b3d8b98527faf
Abdullah <113872995+A4va@users.noreply.github.com>
2023-01-30 16:28:22 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,73 @@
+// JSX
+let root = document.getElementById("root")
+let div = 
+<>
+<header>
+    <div class="block-name">
+        <i class="fa-brands fa-dev"></i>
+        <h1>Abduallah AL-Dawsari</h1>
+        <h3>Junior Wep Devlopment</h3>
+    </div>
+    <div class="block-info">
+        <p>dev.abduallah@gmail.com</p>
+        <p>+996 503337296</p>
+        <p>Saudi Arabia - Riyadh</p>
+    </div>
+</header>
+
+<section>
+    <div>
+        <p>PROFESSIONAL EXPERIENCE</p>
+        <div>
+            <div>
+                <h4>Bootcamp JavaScript By Tuwaiq Academy:</h4>
+                <span>Present</span>
+                <p>It will lead me to build a full-stack web application.</p>
+            </div>
+            <div>
+                <h4>Bootcamp Swaher By Sdaia & Tuwaiq Academy:</h4>
+                <span>2022</span>
+                <p>Fundamentals of smart surveillance camera systems.</p>
+            </div>
+        </div>
+    </div>
+    <div>
+    <p>SKILLS</p>
+        <div>
+            <div>
+                <h4>Programming Languages & Frameworks & Systems:</h4>
+                <ul class="dashed">
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>Bootstrap</li>
+                    <li>Javascript</li>
+                    <li>Git / GitHub</li>
+                    <li>Node.js</li>
+                    <li>TypeScript</li>
+                    <li>Python</li>
+                    <li>C#</li>
+                    <li>PHP</li>
+                    <li>SQL</li>
+                </ul>
+            </div>
+            <div>
+                <h4>Personal:</h4>
+                <ul class="dashed">
+                    <li>Fast Learning</li>
+                    <li>Self-Learning</li>
+                    <li>Problem-Solving</li>
+                    <li>Ambition</li>
+                    <li>Teamwork</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div>
+        <p></p>
+        <p></p>
+    </div>
+</section>
+</>
+
+
+ReactDOM.render(div, root)
